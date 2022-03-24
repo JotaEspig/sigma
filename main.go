e93e8e2c7f1235b8e863f44b73100bd330307f6d
João Vitor Espig <jotinha1300@gmail.com>
2022-03-23 23:49:00 -0300

    Now, it's serves a Hello

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello %s", "Whom")
+	})
+
+	router.Run()
 }
