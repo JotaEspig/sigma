801545395c8fb450b8302e8477e63f587d3929f2
João Vitor Espig <jotinha1300@gmail.com>
2022-03-25 20:05:11 -0300

    The main file serves a html file using a handler function by now

---
+++ handlers/index.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IndexGet() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			nil,
+		)
+	}
+}
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"blue-justice/handlers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello %s", "Whom")
-	})
+	router.LoadHTMLGlob("html/*.html")
+
+	router.GET("/", handlers.IndexGet())
 
 	router.Run()
 }
