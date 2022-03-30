3340a6485f147679920396510eda0afecbbed00c
João Vitor Espig <jotinha1300@gmail.com>
2022-03-30 10:31:27 -0300

    gin.H -> nil

--- handlers/index.go
+++ handlers/index.go
@@ -9,13 +9,7 @@ import (
 func IndexGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.HTML(
-			http.StatusOK,
-			"index.html",
-			gin.H{
-				"Texto": gin.H{
-					"Min": "Teste2",
-				},
-			},
+			http.StatusOK, "index.html", nil,
 		)
 	}
 }
