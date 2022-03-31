0dbe0c371e5543a97841a2e7dc861d1f51cfc63c
João Vitor Espig <jotinha1300@gmail.com>
2022-03-31 09:23:43 -0300

    Started to make the GenerateToken function. Created claims variable

--- services/login/jwt_authetication.go
+++ services/login/jwt_authetication.go
@@ -2,6 +2,7 @@ package login
 
 import (
 	"os"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -43,6 +44,14 @@ func getSecretKey() string {
 
 // Generates a token according to
 func (service *jwtService) GenerateToken(username string) string {
+	claims := &authClaims{
+		username,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 48).Unix(),
+			Issuer:    service.issuer,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
 	return ""
 }
 
