4a145f08d96e1b97678f7bfc66e7fa7f2d61e972
João Vitor Espig <jotinha1300@gmail.com>
2022-04-01 21:59:53 -0300

    Added a function that redirects to login page

--- handlers/login.go
+++ handlers/login.go
@@ -2,9 +2,17 @@ package handlers
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
+
+func LoginRedirect() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		location := url.URL{Path: "/login"}
+		ctx.Redirect(http.StatusFound, location.RequestURI())
+	}
+}
 
 func LoginGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
