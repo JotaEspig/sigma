be9ebe2c4f0b5fbb537594eeac96a83c5004c402
João Vitor Espig <jotinha1300@gmail.com>
2022-04-03 13:33:17 -0300

    Added a function that creates a default userinfo struct

--- services/login/login.go
+++ services/login/login.go
@@ -5,6 +5,13 @@ type UserInfo struct {
 	password string
 }
 
+func DefaultUserInfo() *UserInfo {
+	return &UserInfo{
+		username: "teste",
+		password: "password",
+	}
+}
+
 func (u *UserInfo) CheckLogin(userInput, passInput string) bool {
 	return u.username == userInput && u.password == passInput
 }
