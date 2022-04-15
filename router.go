f7aeee951af96f36de40130abf5288967b3da0fc
João Vitor Espig <jotinha1300@gmail.com>
2022-04-15 15:24:53 -0300

    Added a page for testing the login system

--- router.go
+++ router.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"net/http"
 	"sigma/handlers"
+	"sigma/services/login"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +22,39 @@ func createRouter() *gin.Engine {
 	router.POST("/login", handlers.LoginPOST)
 	// Cadastro
 	router.GET("/cadastro", handlers.SignupGet)
+
+	router.GET("/test", func(ctx *gin.Context) {
+		token, err := ctx.Cookie("auth")
+		if err != nil || token == "" {
+			ctx.HTML(
+				http.StatusUnauthorized,
+				"logintest.html",
+				gin.H{
+					"ServerResponse": "Você não está logado",
+				},
+			)
+			return
+		}
+
+		dtoken, err := login.JWTDefault.ValidateToken(token)
+		if err != nil || !dtoken.Valid {
+			ctx.SetCookie("auth", "", -1, "", "", false, false)
+			ctx.HTML(
+				http.StatusUnauthorized,
+				"logintest.html",
+				gin.H{
+					"ServerResponse": "Você não está logado",
+				},
+			)
+			return
+		}
+
+		ctx.HTML(
+			200,
+			"logintest.html",
+			nil,
+		)
+	})
 
 	return router
 }
