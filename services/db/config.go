d5245981a7bc23f062b837b11f0f97eb7c51eeb3
João Vitor Espig <jotinha1300@gmail.com>
2022-04-21 20:07:11 -0300

    Changed the name of the variable DataSourceName to ConnectionString

--- services/db/config.go
+++ services/db/config.go
@@ -8,12 +8,12 @@ import (
 const PostgresDriver = "postgres"
 
 var (
-	User           string
-	Password       string
-	DbName         string
-	Host           string
-	Port           string
-	DataSourceName string
+	User          string
+	Password      string
+	DbName        string
+	Host          string
+	Port          string
+	ConnectionStr string
 )
 
 func checkEnv(envName string, defaultVal string) string {
@@ -29,6 +29,6 @@ func init() {
 	DbName = checkEnv("DB_DB", "sigma")
 	Host = checkEnv("DB_HOST", "localhost")
 	Port = checkEnv("DB_PORT", "5432")
-	DataSourceName = fmt.Sprintf("host=%s port=%s user=%s "+
+	ConnectionStr = fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
 }
