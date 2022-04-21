fcd04e274e0a5b3aac44ab61d6cba996923b5809
João Vitor Espig <jotinha1300@gmail.com>
2022-04-21 13:59:55 -0300

    Added bcrypt into the login validation

--- services/login/login.go
+++ services/login/login.go
@@ -1,17 +1,37 @@
 package login
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type UserInfo struct {
-	username string
-	password string
+	username       string
+	hashedPassword string
 }
 
 func DefaultUserInfo() *UserInfo {
 	return &UserInfo{
-		username: "teste",
-		password: "password",
+		username:       "teste",
+		hashedPassword: "$2a$10$CsTxuGv/5Y7KUl65AdspPeT1jMjpJePt6Hoi9uKGrsWt3mVdSZK/W",
 	}
 }
 
 func (u *UserInfo) Validate(userInput, passInput string) bool {
-	return u.username == userInput && u.password == passInput
+	err := bcrypt.CompareHashAndPassword([]byte(u.hashedPassword), []byte(passInput))
+	println(len(u.hashedPassword))
+	return u.username == userInput && err == nil
 }
+
+/*password := []byte("MyDarkSecret")
+
+// Hashing the password with the default cost of 10
+hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+if err != nil {
+	panic(err)
+}
+fmt.Println(string(hashedPassword))
+
+// Comparing the password with the hash
+err = bcrypt.CompareHashAndPassword(hashedPassword, password)
+fmt.Println(err) // nil means it is a match
+*/
