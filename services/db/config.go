89b277012037edb8c385bba3cf1b0f2abb364579
João Vitor Espig <jotinha1300@gmail.com>
2022-04-21 14:12:56 -0300

    Added file for database config

---
+++ services/db/config.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"os"
+)
+
+const PostgresDriver = "postgres"
+
+var (
+	User           string
+	Password       string
+	DbName         string
+	Host           string
+	Port           string
+	DataSourceName string
+)
+
+func checkEnv(envName string, defaultVal string) string {
+	if env := os.Getenv(envName); env != "" {
+		return env
+	}
+	return defaultVal
+}
+
+func init() {
+	User = checkEnv("DB_USERNAME", "postgres")
+	Password = checkEnv("DB_PASSWORD", "postgres")
+	DbName = checkEnv("DB_DB", "sigma")
+	Host = checkEnv("DB_HOST", "localhost")
+	Port = checkEnv("DB_PORT", "5432")
+	DataSourceName = fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
+}
