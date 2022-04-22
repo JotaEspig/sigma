119aa15fb96a29326de1800ac63ed11d54020486
João Vitor Espig <jotinha1300@gmail.com>
2022-04-22 16:35:17 -0300

    Changed the way GetUser works and add the function AddUser that inserts a new user to the database

--- services/login/user_dao.go
+++ services/login/user_dao.go
@@ -4,12 +4,23 @@ import "database/sql"
 
 // Gets a user from the database
 func GetUser(db *sql.DB, username string) (*User, error) {
-	var u User
+	u := &User{}
 
 	err := db.QueryRow(
 		"SELECT * FROM \"user\" WHERE \"username\" = $1",
 		username,
-	).Scan(&u.ID, &u.Username, &u.hashedPassword)
+	).Scan(u.ID, u.Username, u.hashedPassword)
 
-	return &u, err
+	return u, err
+}
+
+func AddUser(db *sql.DB, u *User) error {
+	stmt, err := db.Prepare(`INSERT INTO "user"(username, password)
+		VALUES($1, $2)`)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(u.Username, u.hashedPassword)
+	return err
 }
