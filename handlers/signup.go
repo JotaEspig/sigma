a3d43186cae308febe08eb5a5656f525bb7f5a92
João Vitor Espig <jotinha1300@gmail.com>
2022-04-22 23:12:11 -0300

    Added the SignupPOST function already with a doc string

--- handlers/signup.go
+++ handlers/signup.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"net/http"
+	"sigma/services/db"
+	"sigma/services/login"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +14,26 @@ func SignupGET() gin.HandlerFunc {
 		ctx.HTML(
 			http.StatusOK, "cadastro.html", nil,
 		)
+	}
+}
+
+// Gets the values from the form, creates an user and inserts it in the database
+func SignupPOST() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		usern := ctx.PostForm("username")
+		email := ctx.PostForm("email")
+		name := ctx.PostForm("name")
+		passwd := ctx.PostForm("password")
+
+		u := login.InitUser(usern, email, name, passwd)
+		err := login.AddUser(db.DB, u)
+		if err != nil {
+			ctx.JSON(
+				http.StatusConflict, nil,
+			)
+			return
+		}
+
+		ctx.Status(http.StatusOK)
 	}
 }
