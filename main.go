57cd34a6982b642517d8bbaf83926aa9cbc9220c
João Vitor Espig <jotinha1300@gmail.com>
2022-04-24 16:54:45 -0300

    The connection with the database is initialized in the main.go

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "sigma/services/db"
 
 func main() {
+	// Configures and connects to the database
+	db.DB = db.Connect()
 	defer db.DB.Close()
 
+	// Creates and runs the router
 	router := createRouter()
 	router.Run()
 }
--- services/db/connection.go
+++ services/db/connection.go
@@ -6,10 +6,10 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var DB *sql.DB = connect()
+var DB *sql.DB
 
 // Connects with a database
-func connect() *sql.DB {
+func Connect() *sql.DB {
 	driver, connStr := getConfig()
 	db, err := sql.Open(driver, connStr)
 	if err != nil {
