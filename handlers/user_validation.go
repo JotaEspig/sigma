e819af95496633e71441943b041d147cb4b73349
João Vitor Espig <jotinha1300@gmail.com>
2022-05-09 15:11:46 -0300

    Now, sends a JSON with user info

--- handlers/user_validation.go
+++ handlers/user_validation.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	auth "sigma/services/authentication"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -46,10 +47,19 @@ func ValidateUser() gin.HandlerFunc {
 			return
 		}
 
+		user, err := auth.GetUser(db, claims["username"].(string))
+		if err != nil {
+			ctx.Status(http.StatusBadGateway)
+			return
+		}
+
+		userInfo := user.ToMap()
+
 		ctx.JSON(
 			http.StatusOK,
 			gin.H{
 				"claims": claims,
+				"user":   userInfo,
 			},
 		)
 	}
