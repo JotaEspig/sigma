0fe9cf4d1635b5698f1f33bc161d4486a8d51490
João Vitor Espig <jotinha1300@gmail.com>
2022-05-11 11:10:19 -0300

    Now, every html file created will have a route automatically

--- router.go
+++ router.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"os"
+	"path/filepath"
 	"sigma/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -12,6 +15,32 @@ func createRouter() *gin.Engine {
 
 	router.LoadHTMLGlob("html/*.html")
 
+	// These lines add a route to every HTML file inside ./html
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	// Walks inside the folder, checks the filename and then adds as GET
+	filepath.Walk(pwd+"/html/", func(path string, info os.FileInfo, err error) error {
+		if len(info.Name()) < 6 {
+			return nil
+		}
+
+		idxUntilFileExt := len(info.Name()) - 4
+		filePath := "/" + info.Name()
+		filePath = filePath[:idxUntilFileExt]
+
+		router.GET(filePath, func(ctx *gin.Context) {
+			ctx.HTML(
+				http.StatusOK,
+				info.Name(),
+				nil,
+			)
+		})
+		return nil
+	})
+
 	// Loads the css and js folders
 	router.Static("css/", "css/")
 	router.Static("js/", "js/")
@@ -19,23 +48,13 @@ func createRouter() *gin.Engine {
 
 	// Login
 	router.GET("/", handlers.LoginRedirect())
-	router.GET("/login", handlers.LoginGET())
 	router.POST("/login", handlers.LoginPOST())
 
 	// Cadastro
-	router.GET("/cadastro", handlers.SignupGET())
 	router.POST("/cadastro", handlers.SignupPOST())
 
 	// Validate User
 	router.GET("/validate_user", handlers.ValidateUser())
-
-	router.GET("/aluno", func(ctx *gin.Context) {
-		ctx.HTML(
-			200,
-			"aluno.html",
-			nil,
-		)
-	})
 
 	return router
 }
