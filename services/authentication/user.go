4cc0ad0da571b449a5d0624051f6ec07ac59c4ec
João Vitor Espig <jotinha1300@gmail.com>
2022-05-14 12:39:22 -0300

    Changed the name of a field and fixed error in InitUser, it was encrypting an empty string

--- services/authentication/user.go
+++ services/authentication/user.go
@@ -5,21 +5,21 @@ import (
 )
 
 type User struct {
-	ID       int
-	Username string
-	Email    string
-	Name     string
-	Password string
+	ID             int
+	Username       string
+	Email          string
+	Name           string
+	HashedPassword string `db:"password"`
 }
 
-func InitUser(usern, email, name, Password string) *User {
+func InitUser(usern, email, name, password string) *User {
 	u := &User{
 		Username: usern,
 		Email:    email,
 		Name:     name,
 	}
-	passwd, _ := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-	u.Password = string(passwd)
+	hashedPasswd, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	u.HashedPassword = string(hashedPasswd)
 
 	return u
 
@@ -28,7 +28,7 @@ func InitUser(usern, email, name, Password string) *User {
 // Validates the user. It compares the hashed password in the database
 // to the password that the user input
 func (u *User) Validate(userInput, passInput string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(passInput))
+	err := bcrypt.CompareHashAndPassword([]byte(u.HashedPassword), []byte(passInput))
 	return u.Username == userInput && err == nil
 }
 
