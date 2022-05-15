8c76a6aec0c3cb3aecde5a1dee492addcff035e5
João Vitor Espig <jotinha1300@gmail.com>
2022-05-15 20:34:28 -0300

    Updated database

--- services/database/SIGMA.sql
+++ services/database/SIGMA.sql
@@ -1,46 +1,90 @@
-/*
-link for diagram: https://dbdiagram.io/d/62616cbe1072ae0b6ac6170f
-
-Contains the query for the creation of the tables
-To configure the database correctly, you must create these environment variables:
-- DB_USERNAME
-- DB_PASSWORD
-- DB_HOST
-- DB_PORT
-
-And you must run these commands:
-1- Create a database and add its name to a env variable called 'DB_DB'
-2- Access the database (\c <dbname>)
-3- Run these commands (you can just copy and paste)
-
-*If you're using Docker, you don't need to create the env variable, because it
-already exists. So you just need to access the shell of the database container
-and do the rest
-
-** The database is not complete ** 
-
-TODO jota: it's needed to separate the table user to "admin", "teacher", "student"
-
-*/
+CREATE TABLE "class" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar(50) NOT NULL
+);
 
 CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY,
   "username" varchar(50) UNIQUE NOT NULL,
   "password" varchar(60) NOT NULL,
+  "name" varchar(75) NOT NULL,
+  "surname" varchar(50) NOT NULL,
   "email" varchar(40) NOT NULL,
-  "name" varchar(100) NOT NULL
+  "type" varchar(10)
 );
 
-CREATE TABLE "user_class" (
-  "user_id" int,
+CREATE TABLE "admin" (
+  "id" int PRIMARY KEY
+);
+
+CREATE TABLE "student" (
+  "id" int PRIMARY KEY,
+  "year" int,
+  "status" varchar(10) NOT NULL,
   "class_id" int
 );
 
-CREATE TABLE "class" (
-  "id" SERIAL PRIMARY KEY,
-  "subject" varchar(50) NOT NULL
+CREATE TABLE "teacher" (
+  "id" int PRIMARY KEY
 );
 
-ALTER TABLE "user_class" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+CREATE TABLE "subject" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar(50) NOT NULL
+);
 
-ALTER TABLE "user_class" ADD FOREIGN KEY ("class_id") REFERENCES "class" ("id");
+CREATE TABLE "teacher_subject_class" (
+  "teacher_id" int,
+  "subject_id" int,
+  "class_id" int
+);
+
+CREATE TABLE "activity" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar(100) NOT NULL,
+  "description" varchar(1024) NOT NULL
+);
+
+CREATE TABLE "activity_subject_class" (
+  "activity_id" int,
+  "subject_id" int,
+  "class_id" int
+);
+
+CREATE TABLE "news" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar(50) NOT NULL,
+  "description" varchar(1024) NOT NULL
+);
+
+CREATE TABLE "news_subject_class" (
+  "news_id" int,
+  "subject_id" int,
+  "class_id" int
+);
+
+ALTER TABLE "admin" ADD FOREIGN KEY ("id") REFERENCES "user" ("id");
+
+ALTER TABLE "student" ADD FOREIGN KEY ("id") REFERENCES "user" ("id");
+
+ALTER TABLE "student" ADD FOREIGN KEY ("class_id") REFERENCES "class" ("id");
+
+ALTER TABLE "teacher" ADD FOREIGN KEY ("id") REFERENCES "user" ("id");
+
+ALTER TABLE "teacher_subject_class" ADD FOREIGN KEY ("teacher_id") REFERENCES "teacher" ("id");
+
+ALTER TABLE "teacher_subject_class" ADD FOREIGN KEY ("subject_id") REFERENCES "subject" ("id");
+
+ALTER TABLE "teacher_subject_class" ADD FOREIGN KEY ("class_id") REFERENCES "class" ("id");
+
+ALTER TABLE "activity_subject_class" ADD FOREIGN KEY ("activity_id") REFERENCES "activity" ("id");
+
+ALTER TABLE "activity_subject_class" ADD FOREIGN KEY ("subject_id") REFERENCES "subject" ("id");
+
+ALTER TABLE "activity_subject_class" ADD FOREIGN KEY ("class_id") REFERENCES "class" ("id");
+
+ALTER TABLE "news_subject_class" ADD FOREIGN KEY ("news_id") REFERENCES "news" ("id");
+
+ALTER TABLE "news_subject_class" ADD FOREIGN KEY ("subject_id") REFERENCES "subject" ("id");
+
+ALTER TABLE "news_subject_class" ADD FOREIGN KEY ("class_id") REFERENCES "class" ("id");
