f07818c661503a67bf7be4bbd35438e53bd4f3b2
João Vitor Espig <jotinha1300@gmail.com>
2022-05-15 10:52:27 -0300

    Removed useless code, since gin already gets env PORT by itself

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import "os"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
 	// Creates and runs the router
 	router := createRouter()
-	router.Run(":" + port)
+	router.Run()
 }
