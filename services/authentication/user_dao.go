828101a07f671a22b0b42933f7661f111f822a86
João Vitor Espig <jotinha1300@gmail.com>
2022-05-14 21:03:30 -0300

    Added a kind of a filter to get just some of the columns in the database

--- services/authentication/user_dao.go
+++ services/authentication/user_dao.go
@@ -1,16 +1,34 @@
 package auth
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
 // Gets a user from a database
-func GetUser(db *sqlx.DB, username string) (*User, error) {
+func GetUser(db *sqlx.DB, username string, columns ...string) (*User, error) {
+	var columnsStr string
 	u := User{}
 
-	// TODO Jota: Filter results using and array of wanted attributes or whatever
+	// Transforms array of string to string
+	for idx, val := range columns {
+		if idx == len(columns)-1 {
+			columnsStr += val
+			break
+		}
+		columnsStr += val + ","
+	}
+	// If there is no arguments, sets it to default
+	if len(columns) == 0 {
+		columnsStr = "*"
+	}
 
-	err := db.Get(&u, "SELECT * FROM \"user\" WHERE username=$1", username)
+	// TODO Jota: Implement something to "clean" columnsStr
+	// the reason is to avoid SQLInjection (if that's possible)
+	sqlQuery := fmt.Sprintf("SELECT %s FROM \"user\" WHERE username=$1", columnsStr)
+
+	err := db.Get(&u, sqlQuery, username)
 
 	return &u, err
 }
