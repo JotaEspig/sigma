4be7252e84513cfccc87790e41c0927331a807cf
João Vitor Espig <jotinha1300@gmail.com>
2022-05-22 21:37:49 -0300

    Added function ToMap, separated ID from user, added tag to ClassID

--- services/authentication/student/student.go
+++ services/authentication/student/student.go
@@ -2,21 +2,35 @@ package studentauth
 
 import (
 	"database/sql"
+	"errors"
 	userauth "sigma/services/authentication/user"
 )
 
 type Student struct {
+	ID      int
 	User    *userauth.User
 	Year    sql.NullInt16
 	Status  sql.NullString
-	ClassID sql.NullInt64
+	ClassID sql.NullInt64 `db:"class_id"`
 }
 
-func InitStudent(user *userauth.User) *Student {
-	student := &Student{
-		User: user,
+func InitStudent(u *userauth.User) (*Student, error) {
+	if u.ID == 0 {
+		return nil, errors.New("student: ID cannot be 0")
 	}
-	student.User.Type.Scan("student")
 
-	return student
+	s := &Student{
+		ID: u.ID,
+	}
+
+	return s, nil
+}
+
+// Adds student value to map contaning user info
+func (s *Student) ToMap() map[string]interface{} {
+	studentMap := s.User.ToMap()
+	studentMap["year"] = s.Year.Int16
+	studentMap["status"] = s.Status.String
+	studentMap["class_id"] = s.ClassID.Int64
+	return studentMap
 }
