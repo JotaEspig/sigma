8a9fbbc86f41d09975e232658a7529643622a7a3
João Vitor Espig <jotinha1300@gmail.com>
2022-05-25 23:13:19 -0300

    Recreated tests for student

--- tests/student_test.go
+++ tests/student_test.go
@@ -2,101 +2,142 @@ package tests
 
 import (
 	"sigma/db"
+	"sigma/models/student"
 	"sigma/models/user"
 	"testing"
 )
 
 func TestAddStudent(t *testing.T) {
+	db.DB.AutoMigrate(&student.Student{})
+
 	u := user.InitUser(defUsername, defEmail, defName, defSurname, defPasswd)
+
+	// Adds user to be able to use student
 	user.AddUser(db.DB, u)
+
 	u, err := user.GetUser(db.DB, u.Username)
 	if err != nil {
-		t.Error(err)
+		t.Errorf("getting legit user: %s", err)
 	}
 
-	s, err := InitStudent(u)
+	s, err := student.InitStudent(u)
 	if err != nil {
-		t.Error(err)
+		t.Errorf("initializing student: %s", err)
 	}
 
-	func() {
-		defer func() {
-			if r := recover(); r != nil {
-				t.Errorf("adding legit student: %s", r)
-			}
-		}()
-		user.AddStudent(db.DB, s)
-	}()
+	// Adds student without year and status
+	err = student.AddStudent(db.DB, s)
+	if err != nil {
+		t.Errorf("adding legit user (without year and status): %s", err)
+	}
 
-	// repeating the same action
-	func() {
-		defer func() {
-			if r := recover(); r == nil {
-				t.Errorf("adding repeated student (it's not supposed to work)")
-			}
-		}()
-		user.AddStudent(db.DB, s)
-	}()
+	// repeating the same action to check 'unique' columns
+	err = student.AddStudent(db.DB, s)
+	if err == nil {
+		t.Errorf("adding repeated student (it's not supposed to happen): %s", err)
+	}
+
+	err = student.RmStudent(db.DB, s.User.Username)
+	if err != nil {
+		t.Errorf("removing legit user: %s", err)
+	}
+
+	s.Year = 2
+	s.Status = "ativo"
+
+	// Adds user with year and status
+	err = student.AddStudent(db.DB, s)
+	if err != nil {
+		t.Errorf("adding legit user (without year and status): %s", err)
+	}
+
+	err = student.RmStudent(db.DB, s.User.Username)
+	if err != nil {
+		t.Errorf("removing legit user: %s", err)
+	}
+
+	err = student.RmStudent(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit student: %s", err)
+	}
+	err = user.RmUser(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit user: %s", err)
+	}
 }
 
 func TestGetStudent(t *testing.T) {
+	db.DB.AutoMigrate(&student.Student{})
 	u := user.InitUser(defUsername, defEmail, defName, defSurname, defPasswd)
 
 	// Adds if user's not added in the database
-	func() {
-		defer func() {
-			if r := recover(); r != nil {
-				return
-			}
-		}()
-		user.AddUser(db.DB, u)
-	}()
+	user.AddUser(db.DB, u)
 
 	u, err := user.GetUser(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("getting legit user: %s", err)
+	}
+
+	s, err := student.InitStudent(u)
+	if err != nil {
+		t.Errorf("initializing student: %s", err)
+	}
+
+	s.Year = 2
+	s.Status = "ativo"
+
+	// Adds user with year and status
+	err = student.AddStudent(db.DB, s)
+	if err != nil {
+		t.Errorf("adding legit user (without year and status): %s", err)
+	}
+
+	_, err = student.GetStudent(db.DB, u.Username)
 	if err != nil {
 		t.Errorf("getting legit student: %s", err)
 	}
 
-	s, err := InitStudent(u)
-	if err != nil {
-		t.Error(err)
-	}
-
-	s.Year.Scan(2)
-	s.Status.Scan("ativo")
-
-	// Adds student
-	func() {
-		defer func() {
-			if r := recover(); r != nil {
-				t.Error(r)
-			}
-		}()
-		user.AddStudent(db.DB, s)
-	}()
-
-	_, err = GetStudent(db.DB, u.ID)
 	// Checks if get student parcial info is working
+	s, err = student.GetStudent(db.DB, u.Username, "year", "status")
 	if err != nil {
 		t.Errorf("getting legit student (parcial info): %s", err)
 	}
-
-	s, err = GetStudent(db.DB, u.ID, "year", "status")
-	if err != nil {
-		t.Errorf("getting legit student (parcial info): %s", err)
-	}
-	if s.Year.Int16 == 0 {
+	if s.Year == 0 {
 		t.Errorf("getting legit student (parcial info): year is empty")
 	}
-	if s.Status.String == "" {
+	if s.Status == "" {
 		t.Errorf("getting legit student (parcial info): status is empty")
 	}
-	if s.ClassID.Int64 != 0 {
-		t.Errorf("getting legit student (parcial info): class id is filled")
-	}
 
-	_, err = GetStudent(db.DB, nonExistentID)
+	_, err = student.GetStudent(db.DB, "non-existent-username")
 	if err == nil {
 		t.Errorf("getting non existent student (it's not supposed to work): %s", err)
+	}
+
+	err = student.RmStudent(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit student: %s", err)
+	}
+	err = user.RmUser(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit user: %s", err)
+	}
+}
+
+func TestRmStudent(t *testing.T) {
+	db.DB.AutoMigrate(&user.User{})
+
+	u := user.InitUser(defUsername, defEmail, defName, defSurname, defPasswd)
+
+	// Adds if user's not added in the database
+	user.AddUser(db.DB, u)
+
+	err := student.RmStudent(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit student: %s", err)
+	}
+	err = user.RmUser(db.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit user: %s", err)
 	}
 }
