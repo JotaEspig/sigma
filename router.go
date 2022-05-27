2dd389971d9b5073bdc653a4ee983d56dc16b9e9
João Vitor Espig <jotinha1300@gmail.com>
2022-05-27 20:33:13 -0300

    Removed filepath walker

--- router.go
+++ router.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"os"
-	"path/filepath"
 	"sigma/controllers"
 
 	"github.com/gin-gonic/gin"
@@ -12,12 +11,6 @@ import (
 )
 
 // TODO Jota: Set the heroku production and staging
-
-var notToRoute = []string{
-	"alunoinfo.html",
-}
-
-const relativePathToHTML = "/static/html/"
 
 func setNewRelicMiddleware(router *gin.Engine) {
 	nrAppName := os.Getenv("NR_APP_NAME")
@@ -56,60 +49,17 @@ func getRouterEngine() *gin.Engine {
 	return gin.Default()
 }
 
-// Checks if file needs to be route
-// It needed by default, and it's not when the filename is in the notToRoute
-func isHTMLToRoute(filename string, notToRoute []string) bool {
-	for _, val := range notToRoute {
-		if val == filename {
-			return false
-		}
-	}
-	return true
-}
-
-// Gets the route path according to a file information
-func getRoutePath(info os.FileInfo) string {
-	idxUntilFileExt := len(info.Name()) - 4
-	filePath := "/" + info.Name()
-	filePathWithoutExt := filePath[:idxUntilFileExt] // removes the ".html"
-	return filePathWithoutExt
-}
-
 // Set the routes to a router
 func setRoutes(router *gin.Engine) {
 	// TODO Jota: Create groups of routes to separate the route paths
 
-	// TODO Jota: Suggestion for the future: remove the filepath walker
-
-	currentWorkingDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	absPathToHTML := currentWorkingDir + relativePathToHTML
-	// Walks inside the folder, checks the filename and then adds an route for it
-	filepath.Walk(absPathToHTML, func(path string, info os.FileInfo, err error) error {
-		if len(info.Name()) < 6 {
-			return nil
-		}
-		if !isHTMLToRoute(info.Name(), notToRoute) {
-			return nil
-		}
-
-		filePathWithoutExt := getRoutePath(info)
-		router.GET(filePathWithoutExt, func(ctx *gin.Context) {
-			ctx.HTML(
-				http.StatusOK, info.Name(), nil,
-			)
-		})
-		return nil
-	})
-
 	// Login
 	router.GET("/", controllers.LoginRedirect())
+	router.GET("/login", controllers.LoginGET())
 	router.POST("/login", controllers.LoginPOST())
 
 	// Cadastro
+	router.GET("/cadastro", controllers.SignupGET())
 	router.POST("/cadastro", controllers.SignupPOST())
 
 	// Validates User
@@ -119,7 +69,7 @@ func setRoutes(router *gin.Engine) {
 	router.GET("/user/:username", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "alunoinfo.html", nil)
 	})
-	router.POST("/user/:username", controllers.GetUserInfo())
+	router.GET("/user/:username/get", controllers.GetUserInfo())
 }
 
 func createRouter() *gin.Engine {
