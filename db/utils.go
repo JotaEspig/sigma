c7ccb9d85e96afeb979d261ef53301485e48a694
João Vitor Espig <jotinha1300@gmail.com>
2022-05-28 11:00:43 -0300

    rewrited with github copilot :P

--- db/utils.go
+++ db/utils.go
@@ -1,21 +1,19 @@
 package db
 
-// Gets intersection of allowedColumns and columns
+// Gets intersection of allowedColumns and columns. if columns is empty, it returns allowedColumns
 func GetColumns(allowedColumns []string, columns ...string) []string {
-	var columnsToUse []string
-
 	if len(columns) == 0 {
 		return allowedColumns
 	}
 
-	// gets intersection of allowedColumns and columns
-	for _, col := range columns {
-		for _, allowedCol := range allowedColumns {
-			if col == allowedCol {
-				columnsToUse = append(columnsToUse, col)
+	var result []string
+	for _, c := range columns {
+		for _, ac := range allowedColumns {
+			if c == ac {
+				result = append(result, c)
 			}
 		}
 	}
 
-	return columnsToUse
+	return result
 }
