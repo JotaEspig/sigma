cf0b8f974d5c699b2b96153523761b6da07c8096
João Vitor Espig <jotinha1300@gmail.com>
2022-05-28 16:36:15 -0300

    Added file: classroom_dao.go already with add, get and rm

---
+++ models/classroom/classroom_dao.go
@@ -0,0 +1,45 @@
+package classroom
+
+import (
+	dbPKG "sigma/db"
+
+	"gorm.io/gorm"
+)
+
+// Slice containing all classroom params
+var ClassroomParams = []string{
+	"id",
+	"name",
+	"year",
+}
+
+// Slice containing public classroom params
+var PublicClassroomParams = []string{
+	"id",
+	"name",
+	"year",
+}
+
+// Adds a classroom to a database
+func AddClassroom(db *gorm.DB, c *Classroom) error {
+	return db.Create(c).Error
+}
+
+// Gets a classroom from a database
+func GetClassroom(db *gorm.DB, id uint, params ...string) (*Classroom, error) {
+	c := &Classroom{}
+
+	columnsToUse := dbPKG.GetColumns(ClassroomParams, params...)
+
+	err := db.Select(columnsToUse).Where("id = ?", id).First(c).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Removes a classroom from a database
+func RmClassroom(db *gorm.DB, id uint) error {
+	return db.Unscoped().Delete(&Classroom{}, "id = ?", id).Error
+}
