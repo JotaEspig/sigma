9bfdd4bc79750c3006784db333146f66ac178882
João Vitor Espig <jotinha1300@gmail.com>
2022-05-28 12:03:02 -0300

    Added function to serve html files and a function to validate an user with token

--- controllers/user.go
+++ controllers/user.go
@@ -2,29 +2,19 @@ package controllers
 
 import (
 	"net/http"
+	"sigma/config"
 	"sigma/db"
 	"sigma/models/user"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-// Validates an user
-func ValidateUser() gin.HandlerFunc {
+// Serves "alunoinfo.html" page
+func GetUserInfoPage() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		username := ctx.Param("username")
-
-		u, err := user.GetUser(db.DB, username)
-		if err != nil {
-			ctx.AbortWithStatus(http.StatusNotFound)
-			return
-		}
-
-		ctx.JSON(
-			http.StatusOK,
-			gin.H{
-				"user": u.ToMap(),
-			},
-		)
+		ctx.HTML(http.StatusOK, "alunoinfo.html", nil)
 	}
 }
 
@@ -54,4 +44,74 @@ func GetUserInfo() gin.HandlerFunc {
 			},
 		)
 	}
+}
+
+// Serves "aluno.html" page
+func GetAlunoPage() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "aluno.html", nil)
+	}
+}
+
+// Validates an user
+func ValidateUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		username := ctx.Param("username")
+
+		u, err := user.GetUser(db.DB, username)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		ctx.JSON(
+			http.StatusOK,
+			gin.H{
+				"user": u.ToMap(),
+			},
+		)
+	}
+}
+
+// Validates an user with token got from cookie auth
+func ValidateUserWithToken() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token, err := ctx.Cookie("auth")
+		if token == "" || err != nil {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		// check if token is valid
+		dToken, err := config.DefaultJWT.ValidateToken(token)
+		if err != nil || !dToken.Valid {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		claims := dToken.Claims.(jwt.MapClaims)
+
+		now := time.Now().Unix()
+		expiresAt := claims["exp"].(float64)
+		if float64(now) > expiresAt {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		username := claims["username"].(string)
+
+		u, err := user.GetUser(db.DB, username)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		ctx.JSON(
+			http.StatusOK,
+			gin.H{
+				"user": u.ToMap(),
+			},
+		)
+
+	}
+
 }
