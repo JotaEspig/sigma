4a0a34c6f3cdaa408ea58cd49ee2ea4317fdbed9
João Vitor Espig <jotinha1300@gmail.com>
2022-05-29 12:13:06 -0300

    Removed some println

--- tests/jwt_authentication_test.go
+++ tests/jwt_authentication_test.go
@@ -13,11 +13,10 @@ func TestGenerateAndValidate(t *testing.T) {
 		t.Errorf("Error in generating token: %s", err)
 	}
 
-	dToken, err := jwtServ.ValidateToken(eToken)
+	_, err = jwtServ.ValidateToken(eToken)
 	if err != nil {
 		t.Errorf("Error in validating token: %s", err)
 	}
-	println("Token is valid?: ", dToken.Valid)
 
 	// testing with random digits
 	_, err = jwtServ.ValidateToken("dad8i123131")
@@ -34,10 +33,9 @@ func TestGenerateAndValidate(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error in generating fake token (other secret key): %s", err)
 	}
-	dToken, err = jwtServ.ValidateToken(eToken) // Trying to validate token made with the fake
+	dToken, err := jwtServ.ValidateToken(eToken) // Trying to validate token made with the fake
 	if dToken.Valid || err == nil {
 		t.Errorf("Error in validating fake token (other secret key): %s", err)
 	}
-	println("Fake token is valid?: ", dToken.Valid)
 
 }
