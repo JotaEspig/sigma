0b8622ba67dea05241df249bcaa7a2be7fe84ee0
João Vitor Espig <jotinha1300@gmail.com>
2022-05-30 22:51:04 -0300

    Changed the functions name, and added the filter of columns

--- controllers/user.go
+++ controllers/user.go
@@ -12,6 +12,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Serves "aluno.html" page
+func GetAlunoPage() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "aluno.html", nil)
+	}
+}
+
 // Serves "alunoinfo.html" page
 func GetUserInfoPage() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -20,10 +27,10 @@ func GetUserInfoPage() gin.HandlerFunc {
 }
 
 // Gets public user info, according to request
-func GetUserInfo() gin.HandlerFunc {
+func GetPublicUserInfo() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		username := ctx.Param("username")
-		u, err := user.GetUser(db.DB, username, "type")
+		u, err := user.GetUser(db.DB, username, user.PublicUserParams...)
 		if err != nil {
 			ctx.AbortWithStatus(http.StatusNotFound)
 			return
@@ -41,7 +48,9 @@ func GetUserInfo() gin.HandlerFunc {
 
 		switch u.Type {
 		case "student":
-			s, err := student.GetStudent(db.DB, username)
+			s, err := student.GetStudent(db.DB, username,
+				student.PublicStudentParams...)
+
 			if err != nil {
 				ctx.AbortWithStatus(http.StatusNotFound)
 				return
@@ -58,35 +67,49 @@ func GetUserInfo() gin.HandlerFunc {
 	}
 }
 
-// Serves "aluno.html" page
-func GetAlunoPage() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "aluno.html", nil)
-	}
-}
-
-// Validates a user
-func ValidateUser() gin.HandlerFunc {
+// Gets all user info, according to request
+func GetAllUserInfo() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		username := ctx.Param("username")
 
-		u, err := user.GetUser(db.DB, username)
+		u, err := user.GetUser(db.DB, username, user.UserParams...)
 		if err != nil {
 			ctx.AbortWithStatus(http.StatusNotFound)
 			return
 		}
 
-		ctx.JSON(
-			http.StatusOK,
-			gin.H{
-				"user": u.ToMap(),
-			},
-		)
+		if u.Type == "" {
+			ctx.JSON(
+				http.StatusOK,
+				gin.H{
+					"user": u.ToMap(),
+				},
+			)
+			return
+		}
+
+		switch u.Type {
+		case "student":
+			s, err := student.GetStudent(db.DB, username,
+				student.StudentParams...)
+			if err != nil {
+				ctx.AbortWithStatus(http.StatusNotFound)
+				return
+			}
+
+			ctx.JSON(
+				http.StatusOK,
+				gin.H{
+					"user": s.ToMap(),
+				},
+			)
+			return
+		}
 	}
 }
 
 // Validates a user with token got from cookie auth
-func ValidateUserWithToken() gin.HandlerFunc {
+func ValidateUser() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token, err := ctx.Cookie("auth")
 		if token == "" || err != nil {
