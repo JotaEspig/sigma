7a005ea3ec6cf83be1c4e7cd4821ae23ef47900f
João Vitor Espig <jotinha1300@gmail.com>
2022-06-07 13:44:19 -0300

    Now, it checks if username from url is the same of the token

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -11,11 +11,14 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		username := ctx.Param("username")
+
 		token, err := ctx.Cookie("auth")
 		if token == "" || err != nil {
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
+
 		// check if token is valid
 		dToken, err := config.DefaultJWT.ValidateToken(token)
 		if err != nil || !dToken.Valid {
@@ -23,9 +26,16 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
+		claims := dToken.Claims.(jwt.MapClaims)
+
+		if claims["username"] != username {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
 		// checks if token is expired
 		now := time.Now().Unix()
-		expiresAt := dToken.Claims.(jwt.MapClaims)["exp"].(float64)
+		expiresAt := claims["exp"].(float64)
 		if float64(now) > expiresAt {
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
