590d743a7b9db96e870bc54522674cfea266041d
João Vitor Espig <jotinha1300@gmail.com>
2022-06-10 16:27:44 -0300

    Added dao functions

--- models/admin/admin_dao.go
+++ models/admin/admin_dao.go
@@ -1,6 +1,10 @@
 package admin
 
 import (
+	// dbPKG means 'the package db', because if it's named db
+	// it will conflict with db variable in the functions below
+	"sigma/config"
+	dbPKG "sigma/db"
 	"sigma/models/user"
 
 	"gorm.io/gorm"
@@ -32,4 +36,38 @@ func AddAdmin(db *gorm.DB, a *Admin) error {
 
 		return nil
 	})
+}
+
+func GetAdmin(db *gorm.DB, username string, params ...string) (*Admin, error) {
+	a := &Admin{}
+
+	u, err := user.GetUser(db, username, "id")
+	if err != nil {
+		return nil, err
+	}
+
+	columnsToUse := dbPKG.GetColumns(AdminParams, params...)
+
+	err = db.Select(columnsToUse).Where("id = ?", u.ID).First(a).Error
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Model(a).Association("User").Find(&a.User)
+
+	return a, err
+}
+
+func RmAdmin(db *gorm.DB, username string) error {
+	u, err := user.GetUser(db, username, "id")
+	if err != nil {
+		return err
+	}
+
+	return db.Unscoped().Delete(&Admin{UID: u.ID}).Error
+}
+
+// AutoMigrate the admin table
+func init() {
+	config.DB.AutoMigrate(&Admin{})
 }
