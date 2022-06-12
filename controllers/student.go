0f20a36720e201be9da047b680a535a118797e6b
João Vitor Espig <jotinha1300@gmail.com>
2022-06-12 15:10:01 -0300

    added function to get info about student

---
+++ controllers/student.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+	"sigma/config"
+	"sigma/models/student"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetStudentInfo() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		username := ctx.Param("username")
+		s, err := student.GetStudent(config.DB, username)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, s.ToMap())
+	}
+}
