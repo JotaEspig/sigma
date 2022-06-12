aa82a1be3d84df8ee8e30b36fea1708b74891cc9
João Vitor Espig <jotinha1300@gmail.com>
2022-06-12 15:01:32 -0300

    Added function: IsStudentMiddleware

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -44,6 +44,46 @@ func AuthMiddleware() gin.HandlerFunc {
 	}
 }
 
+func IsStudentMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		username := ctx.Param("username")
+		token, err := ctx.Cookie("auth")
+		if token == "" || err != nil {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		// check if token is valid
+		dToken, err := config.JWTService.ValidateToken(token)
+		if err != nil || !dToken.Valid {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		claims := dToken.Claims.(jwt.MapClaims)
+
+		if claims["username"] != username {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		if claims["type"] != "student" {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		// checks if token is expired
+		now := time.Now().Unix()
+		expiresAt := claims["exp"].(float64)
+		if float64(now) > expiresAt {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
 func IsAdminMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		username := ctx.Param("username")
