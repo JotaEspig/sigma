a079dc60c48e38f9212730d22cc4da61419484ea
João Vitor Espig <jotinha1300@gmail.com>
2022-06-15 10:00:48 -0300

    Added UpdateAdmin handler

--- controllers/admin.go
+++ controllers/admin.go
@@ -24,3 +24,25 @@ func GetAdminInfo() gin.HandlerFunc {
 		ctx.JSON(http.StatusOK, a.ToMap())
 	}
 }
+
+func UpdateAdmin() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		newValues := admin.Admin{}
+		username := ctx.Param("username")
+		a, err := admin.GetAdmin(config.DB, username, "id")
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		ctx.ShouldBindJSON(&newValues)
+		newValues.UID = a.UID
+		err = admin.UpdateAdmin(config.DB, &newValues)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.Status(http.StatusOK)
+	}
+}
