a13e09cd5b16478902ad15be3cecf91fadced375
João Vitor Espig <jotinha1300@gmail.com>
2022-07-13 14:57:19 -0300

    added TestRmTeacher

--- tests/teacher_test.go
+++ tests/teacher_test.go
@@ -190,3 +190,45 @@ func TestUpdateTeacher(t *testing.T) {
 		t.Errorf("removing legit user: %s", err)
 	}
 }
+
+func TestRmTeacher(t *testing.T) {
+	u := user.InitUser(defUsername, defEmail, defName, defSurname, defPasswd)
+
+	// Adds user to be able to use teacher
+	user.AddUser(config.DB, u)
+
+	u, err := user.GetUser(config.DB, u.Username)
+	if err != nil {
+		t.Errorf("getting legit user: %s", err)
+	}
+
+	teach, err := teacher.InitTeacher(u)
+	if err != nil {
+		t.Errorf("initializing teacher: %s", err)
+	}
+
+	teach.Education = "Bacharelado"
+
+	// Adds teacher with education
+	err = teacher.AddTeacher(config.DB, teach)
+	if err != nil {
+		t.Errorf("adding legit teacher (with education): %s", err)
+	}
+
+	// Removes teacher
+	err = teacher.RmTeacher(config.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit teacher: %s", err)
+	}
+
+	// Gets teacher from database
+	_, err = teacher.GetTeacher(config.DB, defUsername)
+	if err == nil {
+		t.Errorf("getting legit teacher: %s", err)
+	}
+
+	err = user.RmUser(config.DB, defUsername)
+	if err != nil {
+		t.Errorf("removing legit user: %s", err)
+	}
+}
