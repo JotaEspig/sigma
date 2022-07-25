da75b7b2d7db404375c6e9bad82729492e8015d3
Samuel Candido <samueljosecandido9@gmail.com>
2022-07-25 20:57:19 -0300

    Adicionando funçao para visto na aba principal do aluno

--- static/js/excluir_turma.js
+++ static/js/excluir_turma.js
@@ -14,4 +14,19 @@ $(document).ready(function () {
             tr.remove();  
         });
     });
+    $("#tabela_noticia").on("click", ".remover", function(){
+        let tr = $(this).closest('tr');
+        
+        let confirmado = confirm('Deseja deletar?');
+        if(confirmado){
+            alert('Confirmado!');
+        }else{
+            alert('Negado!'); 
+            return;  
+        }
+
+        tr.fadeOut(400, function() {
+            tr.remove();  
+        });
+    });
 });
