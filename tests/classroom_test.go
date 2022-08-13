2e44d07cb46d75977736176c4e112846bacd27bf
João Vitor Espig <jotinha1300@gmail.com>
2022-08-13 12:11:13 -0300

    added file for test endpoints, not completed

---
+++ tests/classroom_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"sigma/server"
+	"testing"
+)
+
+func TestAddClassroom(t *testing.T) {
+	_ = server.CreateRouter()
+
+	// Test AddClassroom endpoint with http.NewRequest
+	t.Skip("Skipping AddClassroom endpoint test, because it's not implemented yet")
+}
+
+func TestGetClassroom(t *testing.T) {
+	_ = server.CreateRouter()
+
+	// Test GetClassroom endpoint with http.NewRequest
+	t.Skip("Skipping GetClassroom endpoint test, because it's not implemented yet")
+}
+
+func TestGetAllClassrooms(t *testing.T) {
+	_ = server.CreateRouter()
+
+	// Test GetAllClassrooms endpoint with http.NewRequest
+	t.Skip("Skipping GetAllClassrooms endpoint test, because it's not implemented yet")
+}
