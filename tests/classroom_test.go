c0f2dc5a8db21721f581c0197a82912a0d920b8e
João Vitor Espig <jotinha1300@gmail.com>
2022-08-27 16:35:20 -0300

    added test for AddClassroom

--- tests/classroom_test.go
+++ tests/classroom_test.go
@@ -1,15 +1,50 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"sigma/config"
+	"sigma/models/classroom"
 	"sigma/server"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAddClassroom(t *testing.T) {
-	_ = server.CreateRouter()
+	router := server.CreateTestRouter()
 
-	// Test AddClassroom endpoint with http.NewRequest
-	t.Skip("Skipping AddClassroom endpoint test, because it's not implemented yet")
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/login",
+		bytes.NewBuffer([]byte(`username=admin&password=admin`)))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	// Gets json from writer
+	jsonResp := map[string]string{}
+	json.Unmarshal(w.Body.Bytes(), &jsonResp)
+
+	token, ok := jsonResp["token"]
+	assert.Equal(t, true, ok)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/admin/tools/classroom/add",
+		bytes.NewBuffer([]byte(`{"name": "test", "year": 2022}`)))
+	req.AddCookie(&http.Cookie{
+		Name:  "auth",
+		Value: token,
+	})
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
+	err := config.DB.Unscoped().Delete(&classroom.Classroom{}, "name = ?", "test").Error
+	assert.Equal(t, nil, err)
 }
 
 func TestGetClassroom(t *testing.T) {
