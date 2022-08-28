da9cde8523a9bc229f73e83eda46bc840cb1c156
João Vitor Espig <jotinha1300@gmail.com>
2022-08-27 22:43:46 -0300

    added working test functions

--- tests/classroom_test.go
+++ tests/classroom_test.go
@@ -69,16 +69,73 @@ func TestGetClassroom(t *testing.T) {
 		Value: token,
 	})
 
-	err := config.DB.Unscoped().Delete(&classroom.Classroom{}, "name = ?", "test").Error
-	assert.Equal(t, nil, err)
+	router.ServeHTTP(w, req)
+	classrooms := []classroom.Classroom{}
+	json.Unmarshal(w.Body.Bytes(), &classrooms)
 
-	// Test GetClassroom endpoint with http.NewRequest
-	t.Skip("Skipping GetClassroom endpoint test, because it's not implemented yet")
+	classID := 0
+	for _, c := range classrooms {
+		if c.Name == defClassroomName && c.Year == defClassroomYear {
+			classID = int(c.ID)
+			break
+		}
+	}
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(
+		"GET",
+		"/admin/tools/classroom/"+fmt.Sprint(classID)+"/get",
+		nil,
+	)
+	req.AddCookie(&http.Cookie{
+		Name:  "auth",
+		Value: token,
+	})
+
+	router.ServeHTTP(w, req)
+	c := classroom.Classroom{}
+	json.Unmarshal(w.Body.Bytes(), &c)
+	assert.Equal(t, defClassroomName, c.Name)
+	assert.Equal(t, defClassroomYear, c.Year)
+
+	err := config.DB.Unscoped().Delete(&classroom.Classroom{}, "name = ?", defClassroomName).Error
+	assert.Equal(t, nil, err)
 }
 
 func TestGetAllClassrooms(t *testing.T) {
-	_ = server.CreateRouter()
+	router := server.CreateTestRouter()
 
-	// Test GetAllClassrooms endpoint with http.NewRequest
-	t.Skip("Skipping GetAllClassrooms endpoint test, because it's not implemented yet")
+	token, ok := getToken(router, "admin", "admin")
+	assert.Equal(t, true, ok)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(
+		"POST",
+		"/admin/tools/classroom/add",
+		bytes.NewBuffer([]byte(
+			`{"name": "`+defClassroomName+`", "year": `+fmt.Sprint(defClassroomYear)+`}`,
+		)),
+	)
+	req.AddCookie(&http.Cookie{
+		Name:  "auth",
+		Value: token,
+	})
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/admin/tools/classroom/get", nil)
+	req.AddCookie(&http.Cookie{
+		Name:  "auth",
+		Value: token,
+	})
+
+	router.ServeHTTP(w, req)
+	classrooms := []classroom.Classroom{}
+	json.Unmarshal(w.Body.Bytes(), &classrooms)
+	assert.GreaterOrEqual(t, len(classrooms), 1)
+
+	err := config.DB.Unscoped().Delete(&classroom.Classroom{}, "name = ?", defClassroomName).Error
+	assert.Equal(t, nil, err)
 }
