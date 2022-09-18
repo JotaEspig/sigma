ab263f39c964030ae3486b10d866f8fded27928a
João Vitor Espig <jotinha1300@gmail.com>
2022-09-17 23:40:07 -0300

    add todo comment

--- controllers/utils.go
+++ controllers/utils.go
@@ -2,6 +2,9 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
+// TODO Check this function to be compatible with target url param
+// maybe change "target" to "username" in url params
+
 func getUsername(ctx *gin.Context) string {
 	username := ctx.Param("username")
 	if username == "" {
